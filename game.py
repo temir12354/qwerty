ce6354bd42d64ca16b0bbf0999189097b9d0e14e
temir12354 <129775004+temir12354@users.noreply.github.com>
2023-11-13 19:42:50 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,17 @@
 import pygame as pg
-
+import random as r
 pg.init()
 font_name = pg.font.match_font("arial") #поиск шрифта arial
 size = 18 # размер нашего шрофта
 W,H= 600,600
 win = pg.display.set_mode((W,H))
 name = ''
+White= 255,255,255
+eng = [[0]*20 for i in range(20)]
+for i in range(10):
+    row = r.randrange(0,20)
+    col = r.randrange(0,20)
+    eng[col][row]= 2
 def draw_text(surf,text,x,y,size=size,color=(255,255,255)):
     font =pg.font.Font( font_name, size)#определение шрифта
     text_surface = font.render(text,True,color)
@@ -35,14 +41,34 @@ while main_loop:
     draw_text(win,'Введите имя:',W//2,H//2)
     draw_text(win,name,W//2,H//2 +20)
     pg.display.update()
+game = True
+while game:
 
-while True:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             exit()
-        win.fill((255,255,0))
-        for y in range(0,H,30):
+        if i.type == pg.MOUSEBUTTONDOWN:
+            if i.button == 1:
+                x_mouse=i.pos[0]
+                y_mouse=i.pos[1]
+                col = x_mouse // 30
+                row = y_mouse // 30
+                if eng[col][row] == 2:
+                    print('ytuguihiuohoj')
+                    game = False
+                eng[col][row]=1
+                pg.draw.rect(win,White, (i.pos[0],i.pos[1],10,10))
+
+    win.fill((255, 255, 0))
+    for y in range(0,H,30):
             for x in range(0,W,30):
                 pg.draw.line(win,(0,0,0), (0,y),(W,y))
                 pg.draw.line(win,(0,0,0), (x,0),(x,H))
-        pg.display.update()
+    for row in range(20):
+        for col in range(20):
+            if eng[row][col] == 1:
+                pg.draw.rect(win,(0,0,0),(row*30,col*30,30,30))
+    pg.display.update()
+
+#pg.draw.rect()
+#20,20
